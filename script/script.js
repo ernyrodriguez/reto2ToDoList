35a35d9e84e6505303eecb80d270199d4d9e5507
JULIDUARTE <Juianadhiguita@gmail.com>
2023-09-05 14:23:31 -0500

    Agregando arreglo displayitems

--- script/script.js
+++ script/script.js
@@ -121,6 +121,74 @@ function activateCancelListeners() {
     })
   })
 }
+function displayItems() {
+  let items = ''
+  for (let i = 0; i < itemsArray.length; i++) {
+    items += `    <div class="item">
+                    <div class="input-controller">
+                      <input class="toggle" type="checkbox" id="check_${i}" ${
+      itemsArray[i].checked ? 'checked' : ''
+    } />
+                      <textarea disabled>${itemsArray[i].thing}</textarea>
+                      <div class="edit-controller">
+                        <div>
+                          Prioridad
+                          <select id="priority">
+                            <option ${
+                              itemsArray[i].priority === 'Alta'
+                                ? 'selected'
+                                : ''
+                            }>Alta</option>
+                            <option ${
+                              itemsArray[i].priority === 'Media'
+                                ? 'selected'
+                                : ''
+                            }>Media</option> 
+                            <option ${
+                              itemsArray[i].priority === 'Baja'
+                                ? 'selected'
+                                : ''
+                            }>Baja</option> 
+                          </select>
+                        </div>
+                        <div>
+                          Categorías
+                          <select id="category">
+                              <option ${
+                                itemsArray[i].category === 'Casa'
+                                  ? 'selected'
+                                  : ''
+                              }>Casa</option> 
+                              <option ${
+                                itemsArray[i].category === 'Trabajo'
+                                  ? 'selected'
+                                  : ''
+                              }>Trabajo</option> 
+                              <option ${
+                                itemsArray[i].category === 'Emprendimiento'
+                                  ? 'selected'
+                                  : ''
+                              }>Emprendimiento</option> 
+                            </select>
+                        </div>
+                        <i class="fa-solid fa-pen-to-square editBtn"></i>
+                        <i class="fa-solid fa-x deleteBtn"></i>
+                      </div>
+                    </div>
+                    <div class="update-controller">
+                    <button class="saveBtn">Save</button>
+                     <button class="cancelBtn">Cancel</button>
+                    </div>
+                  </div>`
+  }
+  document.querySelector('.todo-list').innerHTML = items
+  activateCheckboxListeners()
+  activateDeleteListeners()
+  activateEditListeners()
+  activateSaveListeners()
+  activateCancelListeners()
+}
+
 
 
 //El sistema debe permitir EDITAR o MODIFICAR una tarea.
@@ -143,4 +211,5 @@ function activateCancelListeners() {
 
 //Recordar llamar las funciones displayItems() y displayFooter() para mostrar
 //las tareas en pantalla
+displayItems() 
 displayFooter()
